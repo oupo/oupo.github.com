35ec0dabe3866116ffd06b22adf8018d5604e9f9
oupo <oupo.nejiki@gmail.com>
2014-10-08 19:55:09 +0900

    avenue-seed-list: 色をつける

--- avenue-seed-list/avenue-seed-list.js
+++ avenue-seed-list/avenue-seed-list.js
@@ -1,6 +1,9 @@
 ﻿/// <reference path="jquery.d.ts" />
 var NAMES = ["ハジメ", "ツギコ", "イチミ", "ミライ", "チユ", "パソキチ", "アートス", "アリョーナ", "アッツォ", "アントニナ", "アルヒッパ", "アリビーナ", "アントン", "アスタ", "アンセルミ", "アンネリ", "アルピ", "アネルマ", "アッラン", "アイニッキ", "アーポ", "アーム", "アクセル", "アッスンタ", "アッサール", "アラベッラ", "アティリオ", "アンネッタ", "アマンド", "アガタ", "ジャコブ", "ジャサント", "ジェローム", "ジョゼ", "ジュスタン", "ジャンヌ", "ジュール", "ジュリー", "ジャスパー", "ジェーン", "ジェヒュー", "ジャニス", "ジョナス", "ジェナ", "ジャレッド", "ジョアン", "ジャン", "ジョイス", "ジョエル", "ジョスリン", "ジョシュア", "ジェシー", "ジェリー", "ジュディス", "チェレン", "ホミカ", "アーティ", "カミツレ", "ヤーコン", "フウロ", "シャガ", "シズイ"];
 
+// くじ屋をやるファンたち (wiki情報)
+var RAFFLE_FUN_NAMES = ["アルヒッパ", "アリビーナ", "アントン", "アスタ", "ジェヒュー", "ジャニス", "ジャサント"];
+
 function listVisitors(seed, visitorsNum, outNum) {
     var res = [];
     var s = seed;
@@ -34,15 +37,35 @@ function onSubmit() {
     var seed = Number(form.elements["seed"].value);
     var visitorsNum = Number(form.elements["visitorsNum"].value);
     var outNum = Number(form.elements["outNum"].value);
+    var coloredNames = form.elements["coloredNames"].value.split(" ");
     var results = listVisitors(seed, visitorsNum, outNum);
+    var idToTds = {};
     var trs = results.map(function (r, i) {
         var $tr = $("<tr>");
         $tr.append($("<th>").text(i));
         $tr.append($("<td>").text(hex(r.seed)));
         $tr.append($("<td>").text(r.gymleader_rand));
         $tr.append($("<td>").text(NAMES[r.gymleader]));
-        r.visitors.forEach(function (visitor) {
-            $tr.append($("<td>").text(NAMES[visitor]));
+        r.visitors.forEach(function (visitor, idx) {
+            var $td = $("<td>").text(NAMES[visitor]);
+            if (RAFFLE_FUN_NAMES.indexOf(NAMES[visitor]) >= 0) {
+                $td.addClass("raffle");
+            }
+            if (coloredNames.indexOf(NAMES[visitor]) >= 0) {
+                $td.addClass("colored");
+            }
+            if (r.visitors.indexOf(visitor) < idx) {
+                $td.addClass("skipped");
+            }
+            if (!idToTds[visitor])
+                idToTds[visitor] = [];
+            idToTds[visitor].push($td.get(0));
+            $td.hover(function () {
+                $(idToTds[visitor] || []).addClass("hover");
+            }, function () {
+                $(idToTds[visitor] || []).removeClass("hover");
+            });
+            $tr.append($td);
         });
         return $tr;
     });
--- avenue-seed-list/avenue-seed-list.ts
+++ avenue-seed-list/avenue-seed-list.ts
@@ -2,6 +2,9 @@
 
 var NAMES = ["ハジメ", "ツギコ", "イチミ", "ミライ", "チユ", "パソキチ", "アートス", "アリョーナ", "アッツォ", "アントニナ", "アルヒッパ", "アリビーナ", "アントン", "アスタ", "アンセルミ", "アンネリ", "アルピ", "アネルマ", "アッラン", "アイニッキ", "アーポ", "アーム", "アクセル", "アッスンタ", "アッサール", "アラベッラ", "アティリオ", "アンネッタ", "アマンド", "アガタ", "ジャコブ", "ジャサント", "ジェローム", "ジョゼ", "ジュスタン", "ジャンヌ", "ジュール", "ジュリー", "ジャスパー", "ジェーン", "ジェヒュー", "ジャニス", "ジョナス", "ジェナ", "ジャレッド", "ジョアン", "ジャン", "ジョイス", "ジョエル", "ジョスリン", "ジョシュア", "ジェシー", "ジェリー", "ジュディス", "チェレン", "ホミカ", "アーティ", "カミツレ", "ヤーコン", "フウロ", "シャガ", "シズイ"];
 
+// くじ屋をやるファンたち (wiki情報)
+var RAFFLE_FUN_NAMES = ["アルヒッパ", "アリビーナ", "アントン", "アスタ", "ジェヒュー", "ジャニス", "ジャサント"];
+
 interface Result {
     seed: number;
     gymleader_rand: number;
@@ -41,15 +44,34 @@ function onSubmit() {
     var seed = Number(form.elements["seed"].value);
     var visitorsNum = Number(form.elements["visitorsNum"].value);
     var outNum = Number(form.elements["outNum"].value);
+    var coloredNames = form.elements["coloredNames"].value.split(" ");
     var results = listVisitors(seed, visitorsNum, outNum);
+    var idToTds = {};
     var trs = results.map((r, i) => {
         var $tr = $("<tr>");
         $tr.append($("<th>").text(i));
         $tr.append($("<td>").text(hex(r.seed)));
         $tr.append($("<td>").text(r.gymleader_rand));
         $tr.append($("<td>").text(NAMES[r.gymleader]));
-        r.visitors.forEach(function (visitor) {
-            $tr.append($("<td>").text(NAMES[visitor]));
+        r.visitors.forEach(function (visitor, idx) {
+            var $td = $("<td>").text(NAMES[visitor]);
+            if (RAFFLE_FUN_NAMES.indexOf(NAMES[visitor]) >= 0) {
+                $td.addClass("raffle");
+            }
+            if (coloredNames.indexOf(NAMES[visitor]) >= 0) {
+                $td.addClass("colored");
+            }
+            if (r.visitors.indexOf(visitor) < idx) {
+                $td.addClass("skipped");
+            }
+            if (!idToTds[visitor]) idToTds[visitor] = [];
+            idToTds[visitor].push($td.get(0));
+            $td.hover(() => {
+                $(idToTds[visitor] || []).addClass("hover");
+            }, () => {
+                $(idToTds[visitor] || []).removeClass("hover");
+            });
+            $tr.append($td);
         });
         return $tr;
     });
