73b8b7365f0015657a2a013badd9a92e190260e1
oupo <oupo.nejiki@gmail.com>
2020-01-04 12:46:37 +0900

    ライセンス表記

---
+++ footer-poke.js
@@ -0,0 +1,11 @@
+document.write(`
+<div style="background-color:#ddd;color:#666;font-size:small;">
+<p>
+ポケットモンスター・ポケモン・Pokémonは任天堂・クリーチャーズ・ゲームフリークの登録商標です。
+当ツールは <a href="http://oupo.github.io/profile.html">oupo</a> 個人により運営されており、株式会社ポケモン及びその関連会社とは一切関係ありません。
+</p>
+<p>
+ポケモンに関するコンテンツの一切の権利は株式会社ポケモン、及び関連会社に帰属します。コンテンツの扱いには細心の注意を払っておりますが、万一ご意見・ご要望等があれば oupo.nejiki@gmail.com までご連絡下さい。
+なお、当ツール自体の著作権は oupo に帰属します。
+</p>
+`);
